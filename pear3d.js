8014a3ec64df0f3f12e91088219ceadab6c4d349
laenchan <127580343+laenchan@users.noreply.github.com>
2023-04-16 14:29:33 -0500

    Update pear3d.js
    Signed-off-by: laenchan <127580343+laenchan@users.noreply.github.com>

--- pear3d.js
+++ pear3d.js
@@ -2,16 +2,16 @@ let peary;
 let UV;
 
 function preload() {
-  peary = loadModel('peary333d.obj', true);
-  UV = loadImage('UVPEAR.png');
+  //peary = loadModel('peary333d.obj', true);
+  //UV = loadImage('UVPEAR.png');
 }
 function setup() {
   createCanvas(320, 370, WEBGL); 
   pixelDensity(0.4);
 }
 function draw() {
-  stroke(0);
-  strokeWeight(0);
+  //stroke(0);
+  //strokeWeight(0);
   background(250, 245, 235);
   orbitControl(5,5,0);
   scale(1.5);
@@ -19,6 +19,7 @@ function draw() {
  
   rotateX(3.1);
   rotateY(4);
-  texture(UV);
-  model(peary);
+  box(100,200);
+  //texture(UV);
+  //model(peary);
 }

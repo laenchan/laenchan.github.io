7be1ccf7729ea4eff461f639c36cc72aaa161c7e
laenchan <127580343+laenchan@users.noreply.github.com>
2023-04-16 02:16:30 -0500

    Update pear3d.js
    Signed-off-by: laenchan <127580343+laenchan@users.noreply.github.com>

--- pear3d.js
+++ pear3d.js
@@ -5,10 +5,10 @@ function preload() {
 }
 function setup() {
   createCanvas(500, 500, WEBGL);
-  pixelDensity(0.3);
+  //pixelDensity(0.3);
 }
 function draw() {
-  background(255, 245, 235);
+  background(250, 245, 235);
   orbitControl(5,5,0);
   rotateY(2.5);
   model(peary);

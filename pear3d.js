ccec6668dc37fc3abf37d67e09e684effd49b1ee
laenchan <127580343+laenchan@users.noreply.github.com>
2023-04-16 01:43:33 -0500

    Create pear3d.js
    Signed-off-by: laenchan <127580343+laenchan@users.noreply.github.com>

---
+++ pear3d.js
@@ -0,0 +1,16 @@
+let peary;
+
+function preload() {
+  peary = loadModel('/docs/assets/peary.obj');
+}
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  pixelDensity(0.3);
+  normalMaterial();
+}
+function draw() {
+  background(250, 245, 235);
+  orbitControl(5,5,0);
+  rotateY(2.5);
+  model(peary);
+}
